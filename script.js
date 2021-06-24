5fde546316bd82894fa09c2524ff0d7e5dee0bc4
David Amoateng <amoat7@gmail.com>
2021-06-25 09:54:27 +1000

    commit

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ window.addEventListener('keydown', keypress)
 
 
 
+
 const keys = document.querySelectorAll('.key')
 function transitionEnd(e){
     this.classList.remove('playing')
@@ -23,4 +24,22 @@ function transitionEnd(e){
 
 keys.forEach(key=>{
     key.addEventListener('animationend', transitionEnd)
+})
+
+function keyclick(e){
+    const audio = document.querySelector(`audio[data-key="${this.getAttribute('data-key')}"]`)
+
+    if(!audio){
+        return
+    }
+    audio.currentTime = 0;
+    audio.play()
+    this.classList.add('playing')
+   
+    
+}
+
+
+keys.forEach(key=>{
+    key.addEventListener('click', keyclick)
 })
